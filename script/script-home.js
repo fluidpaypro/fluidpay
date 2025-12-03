2229bd612155a06111644d4374be0eb788fa4c5f
johnslvnjohn <johnslvnjohn@gmail.com>
2025-12-03 19:58:00 +0000

    google verification meta added to index

--- script/script-home.js
+++ script/script-home.js
@@ -1,3 +1,6 @@
+//Java script created by John Selvin John for the home page//
+
+//below shows the array of messages created to be circulated in the homepage as an overlay to the background video - https://www.w3schools.com/js/js_arrays.asp//
 const titleMessages = ["In a world that moves faster than ever…", 
     "payments shouldn’t slow you down", 
     "Introducing FluidPay", 
@@ -9,23 +12,23 @@ const titleMessages = ["In a world that moves faster than ever…",
     "flowing wherever business takes you.",
     "Payments, Redefined."];
 
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded",() => { //DOMContentLoaded is used to ensure that the event is triggered only after the content is fully loaded// 
     const heroEl = document.getElementById("titleMessages");
-    if (!heroEl) return;
 
     const durations = [
-        3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 4000
+        3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 4000 //to specify the timing for each of the message - https://www.w3schools.com/js/js_timing.asp//
     ]
 
-    let index = 0;
+    let index = 0;// to start with first message from titlemessages array
 
     function showtitleMessages(){
-        heroEl.classList.remove("fade-in-up");
+        heroEl.classList.remove("fade-in-up");//to clear the previous animation
         void heroEl.offsetWidth;
 
-        heroEl.textContent=titleMessages[index];
+        heroEl.textContent=titleMessages[index];//helps to loop through the list of messages in the array
 
-        heroEl.classList.add("fade-in-up");
+        heroEl.classList.add("fade-in-up");//loads the animation again
     }
 
     function scheduleNext(){
